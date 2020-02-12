2dd49d7e151c12f02e24856dcf771c15d946b954
Lacey Rainbolt <jlrainbolt@northwestern.edu>
2020-02-11 18:01:24 -0600

    Started QM HW 2

--- 335/Python/entry.py
+++ 335/Python/entry.py
@@ -1,30 +1,33 @@
 import tkinter as tk
 
-def show_entry_fields():
-    print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
+fields = 'Last Name', 'First Name', 'Job', 'Country'
 
-master = tk.Tk()
-tk.Label(master, 
-         text="First Name").grid(row=0)
-tk.Label(master, 
-         text="Last Name").grid(row=1)
+def fetch(entries):
+    for entry in entries:
+        field = entry[0]
+        text  = entry[1].get()
+        print('%s: "%s"' % (field, text)) 
 
-e1 = tk.Entry(master)
-e2 = tk.Entry(master)
+def makeform(root, fields):
+    entries = []
+    for field in fields:
+        row = tk.Frame(root)
+        lab = tk.Label(row, width=15, text=field, anchor='w')
+        ent = tk.Entry(row)
+        row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+        lab.pack(side=tk.LEFT)
+        ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+        entries.append((field, ent))
+    return entries
 
-e1.grid(row=0, column=1)
-e2.grid(row=1, column=1)
+if __name__ == '__main__':
+    root = tk.Tk()
+    ents = makeform(root, fields)
+    root.bind('', (lambda event, e=ents: fetch(e)))   
+    b1 = tk.Button(root, text='Show',
+                  command=(lambda e=ents: fetch(e)))
+    b1.pack(side=tk.LEFT, padx=5, pady=5)
+    b2 = tk.Button(root, text='Quit', command=root.quit)
+    b2.pack(side=tk.LEFT, padx=5, pady=5)
+    root.mainloop()
 
-tk.Button(master, 
-          text='Quit', 
-          command=master.quit).grid(row=3, 
-                                    column=0, 
-                                    sticky=tk.W, 
-                                    pady=4)
-tk.Button(master, 
-          text='Show', command=show_entry_fields).grid(row=3, 
-                                                       column=1, 
-                                                       sticky=tk.W, 
-                                                       pady=4)
-
-tk.mainloop()
